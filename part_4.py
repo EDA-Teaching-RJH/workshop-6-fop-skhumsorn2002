c7141037273606aceaa314d6cfd73c2f923026c5
skhumsorn2002 <sk2007@kent.ac.uk>
2026-02-09 13:45:37 +0000

    part 4 task

--- part_4.py
+++ part_4.py
@@ -1,1 +1,24 @@
+travel_log = []
 
+while True:
+    while True:
+        try:
+          reading = int(input("Sensor Reading (Slope Angle): "))
+        except ValueError:
+            print("Sensor Glitch")
+        else:
+            break
+
+    if reading > 45:
+        print("CRITICAL TILT! HALTING.")
+        break
+    else:
+        travel_log.append(reading)
+        print("Path Stable. Moving forward.")
+        continue
+
+print("Mission Terminated")
+print(F"Total step taken: {len(travel_log)}")
+
+for i in range(len(travel_log)):
+    print(F"Log {i+1} angel: {travel_log[i]}")
