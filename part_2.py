827699eb9bd51320d292a6b3a9f69f518af7649d
skhumsorn2002 <sk2007@kent.ac.uk>
2026-02-09 12:24:52 +0000

    Part 2 task

--- part_2.py
+++ part_2.py
@@ -1,1 +1,22 @@
+rover_status = {
+    "Battery":100  ,
+    "Heater": "Off",
+    "Camera": "Standby",
+}
 
+mission_log = {
+    "Site_1":{"Site": "Crater A", "Radiation": "Low", "Water": False},
+    "Site_2":{"Site": "Dune B", "Radiation": "High", "Water": True}
+}
+
+print(rover_status["Battery"])
+
+rover_status["Battery"] = 80
+rover_status["Heater"] = "On"
+
+rover_status["Speed"] = 5
+
+print(rover_status)
+
+for x in mission_log:
+    print (F"Site {mission_log[x]["Site"]} has {mission_log[x]["Radiation"]} radiation.")
