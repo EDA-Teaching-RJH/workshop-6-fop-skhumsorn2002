1bc42e61d447241b788f5fb180370d79b8bd339a
skhumsorn2002 <sk2007@kent.ac.uk>
2026-02-09 13:20:50 +0000

    part 3 task

--- part_3.py
+++ part_3.py
@@ -1,1 +1,26 @@
+while True:
+    try:
+        speed = int(input("Enter Motor Speed:"))
+    except ValueError :
+        print ("Error: Corrupted Signal. Maintaining current speed.")
+    else:
+        break
 
+def main():
+    while True:
+        coordinate = get_coordinate()
+        if coordinate < -100 or coordinate > 100:
+            print("Coordinate out of range")
+        else:
+            print(F"Set Coordinate to {coordinate}")
+            break
+
+def get_coordinate():
+    while True:
+        try:
+            x_coordinate = int(input("Enter an X-coordinate: "))
+            return x_coordinate
+        except ValueError:
+            print("Invalid coordinate")
+
+main()
