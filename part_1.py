8b1862d34b45ac9c1741ae9a1b94b1e6173a1e34
skhumsorn2002 <sk2007@kent.ac.uk>
2026-02-09 11:44:58 +0000

    part 1 task

--- part_1.py
+++ part_1.py
@@ -1,1 +1,26 @@
+sample_bay = ["Basalt", "Silica", "Iron", "Dust"]
+total_sample = len(sample_bay)
 
+print(sample_bay[0])
+print(sample_bay[total_sample-1])
+print(f"Total number of sample is {total_sample}")
+
+for sample in sample_bay:
+    print(f"Transmitting data for: {sample}")
+
+new_findings = []
+
+new_sample_number = int(input("How many new finding: "))
+
+for i in range(new_sample_number):
+    finding_item = input(f"Enter item number {i+1} name: ").title()
+    new_findings.append(finding_item)
+
+print(new_findings)
+
+if "Dust" in sample_bay:
+    sample_bay.remove("Dust")
+else:
+    pass
+
+print(sample_bay)
